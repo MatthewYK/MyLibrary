db47fc1c3b74a2bffadb94c39de40278f6b9e948
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-19 22:13:56 -0500

    Employee

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GetInt()
+        {
+            return 5;
+        }
+        public virtual decimal CalculateMonthlyPayment()
+        {
+            return 12000;
+        }
+    }
+    public class Secretary : Employee
+    {
+        public override decimal CalculateMonthlyPayment()
+        {
+            return 15000;
+        }
+    }
+    public class Manager : Employee
+    {
+        public override decimal CalculateMonthlyPayment()
+        {
+            return 30000;
+        }
+
+    }
+    public class Developer : Employee
+    {
+
+    }
+}
