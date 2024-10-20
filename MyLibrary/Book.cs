b1b77d204957cd95b492ae38d0030cc73b19897b
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-19 22:13:34 -0500

    Book

---
+++ MyLibrary/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Book
+    {
+        public string Title { get; }
+        public Book (int id, string name, decimal unitPrice, int unitsInStock,string title) :
+            base(id, name, unitPrice, unitsInStock)
+        {
+            Title = title;
+        }
+    }
+}
