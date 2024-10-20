497a2ad5b13da751f8ea20cb368e01e9c027dec6
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-19 21:52:05 -0500

    Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyLibrary;
+using System.Net.Http.Headers;
+int i = 5;
+string Name = "Hello";
+
+Product Leche;
+Leche = new Product(1,"Leche entera",19,100);
